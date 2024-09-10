ba4a46a2655e94558a69a4af266f1bf72818d036
unknown <fajardillah25@gmail.com>
2024-09-10 18:34:08 +0700

    Upload

---
+++ belajar-java-datetime/src/main/java/core-java-datetime/App.java
@@ -0,0 +1,13 @@
+package core-java-datetime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
