f1b2517a2e9ff0f970202080406fd34b1f0d3c3a
fajar abdillah <fajardillah25@gmail.com>
2025-01-21 22:26:45 +0700

    Upload

---
+++ LogicProgrammingTest/src/main/java/hackerrank/PlusMinusZero.java
@@ -0,0 +1,17 @@
+package hackerrank;
+
+
+/**
+ * misal ada sebuah arr[n] = [2,2,0,-1,-1]
+ * hasil output harus bilangand desimal dengan 6 angka di belakang koma (ex = 0.400000, 0.600000), yang didapet dari element array / size array
+ * hasil cetak setiap array harus ditulis dengan baris baru
+ * number harus mempunyai jumlah bilangan positif dan negatif yang sama  dan harus ada 0
+ *
+ */
+
+public class PlusMinusZero {
+
+
+
+
+}
