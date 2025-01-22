53cf28709599ed3620caf130feec6d432e2687d2
fajar abdillah <fajardillah25@gmail.com>
2025-01-22 10:56:25 +0700

    Upload

--- LogicProgrammingTest/src/main/java/hackerrank/PlusMinusZero.java
+++ LogicProgrammingTest/src/main/java/hackerrank/PlusMinusZero.java
@@ -1,6 +1,8 @@
 package hackerrank;
 
 
+import java.util.List;
+
 /**
  * misal ada sebuah arr[n] = [2,2,0,-1,-1]
  * hasil output harus bilangand desimal dengan 6 angka di belakang koma (ex = 0.400000, 0.600000), yang didapet dari element array / size array
@@ -11,7 +13,21 @@ package hackerrank;
 
 public class PlusMinusZero {
 
+    public static void plusMinus(List<Integer> arr){
+        int n = arr.size();
+        long positive = arr.stream().filter(count -> count > 0).count();
+        long negative = arr.stream().filter(count -> count < 0).count();
+        long zero = arr.stream().filter(count -> count == 0).count();
 
+        System.out.printf("%.6f%n", (double) positive/n);
+        System.out.printf("%.6f%n", (double) negative/n);
+        System.out.printf("%.6f%n", (double) zero/n);
+
+    }
+
+    public static void main(String[] args) {
+        plusMinus(List.of(6));
+    }
 
 
 }
