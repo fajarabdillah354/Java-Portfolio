9f5dd408e8f6b62b2f9b85e005fd62dc111fb2fb
fajar abdillah <fajardillah25@gmail.com>
2025-01-22 16:46:44 +0700

    Updated Reverse String

--- LogicProgrammingTest/src/main/java/hackerrank/ReverseString.java
+++ LogicProgrammingTest/src/main/java/hackerrank/ReverseString.java
@@ -1,4 +1,23 @@
 package hackerrank;
 
 public class ReverseString {
+
+    //buat fungsi yang digunakan untuk membalikkan string dengan StringBuilder/StringBuffer
+
+    public static boolean isAnagram(String s){
+        StringBuffer stringBuffer = new StringBuffer(s);
+        stringBuffer.reverse();
+        String reverse = stringBuffer.toString();
+        if (reverse.equals(s)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println( isAnagram("kodok"));
+    }
+
+
 }
