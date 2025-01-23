4561ca76156936c7936ec51ac1cd8f8f5680eb60
fajar abdillah <fajardillah25@gmail.com>
2025-01-23 11:29:31 +0700

    add requerement

--- LogicProgrammingTest/src/main/java/hackerrank/SpringBootChallange.java
+++ LogicProgrammingTest/src/main/java/hackerrank/SpringBootChallange.java
@@ -2,6 +2,13 @@ package hackerrank;
 
 public class SpringBootChallange {
 
+    /*
+    Buat Rest API CRUD untuk entity product dengan
+    - Validasi input
+    - Exeption handling
+    - Unit Testing
+    - Dokumentasi with swagger
+     */
 
 
 }
