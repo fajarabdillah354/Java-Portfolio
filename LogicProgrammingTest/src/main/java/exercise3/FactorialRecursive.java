56c02d87e2ba4d891fb61a08c8809e06344ba8c6
fajar abdillah <fajardillah25@gmail.com>
2025-01-25 22:28:09 +0700

    Upload

---
+++ LogicProgrammingTest/src/main/java/exercise3/FactorialRecursive.java
@@ -0,0 +1,21 @@
+package exercise3;
+
+public class FactorialRecursive {
+
+
+
+    static int factorial(int n){
+        if (n == 0 || n == 1){
+            return 1;
+        }else {
+            return n * factorial(n-1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(3));
+    }
+
+
+}
